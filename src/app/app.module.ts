93a63e1c0134b860f65635427cd993dacfeaa686
Timur Tuleuov <timmmur123@gmail.com>
2022-09-19 00:56:20 +0600

    project before express

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoComponent } from './components/todo/todo.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatListModule,
+    MatInputModule,
+    MatProgressBarModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+ 
+  name = '';
+  time = '';
+  todoList: string[] = [''];
+  isAdd = false;
+  isDel = false;
+  constructor() { }
+  // todoAdd(value){
+  //   this.todoList.unshift(value);
+  //   return false;
+  // }
+  openAdd(){
+    this.isAdd = !this.isAdd;
+  }
+  openDel(){
+    this.isDel = !this.isDel;
+  }
+  
+  ngOnInit(){
+    this.todoList = ['помыть кошачьи миски'];
+  }
+  todoAdd(opt: string){
+    this.todoList.unshift(opt);
+    return false;
+  }
+  todoDel(opt: string){
+    for (let i =0; i<this.todoList.length; i++){
+        if (this.todoList[i] == opt){
+          this.todoList.splice(i, 1);
+          break;
+        }
+    }
+  }
+}
